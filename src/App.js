cb82c7974e1ca477d7694ec5a439724daaac487e
Oleksandr Kozhemiako <alekskojem@gmail.com>
2025-10-11 21:50:26 +0000

    Initial implementation of the Horseradish-GPT application with basic structure in App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+ReadableStreamDefaultController
+import React from "react";
+
+const App() {
+  return (
+    <div>
+        <h1>Horseradish-GPT</h1>
+        <p>Hello World</p>
+    </div>
+  );
+};
+export default App;
