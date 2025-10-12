4c8098646de20a231aa216553b76c910264add57
Alekskojem <alexkojem@gmail.com>
2025-10-12 20:09:48 +0300

    change files index.js index.html App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import React from "react";
+import React from 'react';
 
 const App = () => {
   return (
     <div>
         <h1>Horseradish-GPT</h1>
-
     </div>
-  );
-};
+  )
+}
+export default App
